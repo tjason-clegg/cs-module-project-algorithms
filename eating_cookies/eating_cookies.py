a795bcd09285a18984b756d48e0c359b999216b6
Jason Clegg <tjason.clegg@gmail.com>
2020-09-16 22:54:37 -0600

    Submitting for day 1 MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,25 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+# Recursion should be effective
+
+
+def eating_cookies(n, b=None):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        a = int(n - 1)
+        b = int(n - 2)
+        c = int(n - 3)
+        return eating_cookies(a) + eating_cookies(b) + eating_cookies(c)
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,21 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    temp = []
+
+    # Loop over arr
+    for i in arr:
+        # Check to see if the index isnt in the temp list
+        if i not in temp:
+            # If it isnt, append in to temp list
+            temp.append(i)
+        else:
+            # Otherwise, remove the index from temp
+            temp.remove(i)
+    return temp[0]
 
 
 if __name__ == '__main__':
